678e7fec6ddb05aa210fd3586c2dd871cc3e662d
prosoftwaredev <final.fantasy.dev@gmail.com>
2020-11-13 20:09:30 -0400

    fix conflicts

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
 
+  if (req.url === '/user-agent') return respondUserAgent(req, res)
+
   res.end()
 })
 
@@ -16,6 +18,11 @@ function respondHello (req, res) {
 function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
+}
+
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+  res.end(JSON.stringify({ ua }))
 }
 
 server.listen(PORT)
